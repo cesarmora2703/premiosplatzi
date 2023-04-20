255c8ca72a2b4d91730931fb0d092fb388a4130c
cesarmora2703 <cesarmora2703@gmail.com>
2023-04-20 13:52:04 -0500

    New text for combinations of past and future questions

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -7,37 +7,54 @@ from django.urls import reverse
 from .models import Question
 # Create your tests here.
 
-#Testing recent posts published between a tionme window of one day.
+# Testing recent posts published between a tionme window of one day.
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_questions(self):
         '''This method return false for questions whose pub_date is in the future.'''
         # One month in the future
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
+        future_question = Question(
+            question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
         # Published tomorrow
         time = timezone.now() + datetime.timedelta(days=1)
-        future_question = Question(question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
+        future_question = Question(
+            question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_today_question(self):
         '''This method return false for questions whose pub_date is today.'''
         # Right now
         time = timezone.now()
-        today_question = Question(question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
+        today_question = Question(
+            question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
         self.assertIs(today_question.was_published_recently(), True)
 
     def test_was_published_recently_with_past_questions(self):
         '''This method return false for questions whose pub_date is in the future.'''
         # One month ago
         time = timezone.now() + datetime.timedelta(days=-30)
-        past_question = Question(question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
+        past_question = Question(
+            question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
         self.assertIs(past_question.was_published_recently(), False)
         # One day ago
         time = timezone.now() + datetime.timedelta(days=-1)
-        past_question = Question(question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
+        past_question = Question(
+            question_text='¿Quien es el mejor Course director de Platzi?', pub_date=time)
         self.assertIs(past_question.was_published_recently(), False)
+
+
+def create_question(question_text, days):
+    '''
+    Create a question with the given question test,
+    ans published the number of days offset to now,
+    negative for question in the past,
+    positive for question that will be published later.
+    '''
+    time = timezone.now() + datetime.timedelta(days)
+    return Question(question_text=question_text, pub_date=time).save()
 
 
 class QuestionIndexViewTest(TestCase):
@@ -48,31 +65,70 @@ class QuestionIndexViewTest(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context["latest_question_list"], [])
 
-
     def test_no_questions_from_the_future(self):
         '''No question from the future must be displayed in /polls/'''
         # Published tomorrow
-        time = timezone.now() + datetime.timedelta(days=1)
-        q = Question(question_text='Question from the future', pub_date=time).save()
+        """ time = timezone.now() + datetime.timedelta(days=1)
+        q = Question(question_text='Question from the future', pub_date=time).save() """
+        q = create_question('Question from the future', 1)
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         # Future from the future shouldn't be in response context
         self.assertNotContains(response, 'Question from the future')
-        
-        
+
     def test_questions_from_right_now(self):
-        #Now
+        # Now
         time = timezone.now()
-        qnow = q = Question(question_text='Question right now', pub_date=time).save()
+        qnow = q = Question(
+            question_text='Question right now', pub_date=time).save()
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Question right now')
 
-    
     def test_questions_from_more_than_one_day_old(self):
         # Yesterday, question from past must appears
         time = timezone.now() + timezone.timedelta(days=-1)
-        qnow = q = Question(question_text='Question from past', pub_date=time).save()
+        qnow = q = Question(
+            question_text='Question from past', pub_date=time).save()
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Question from past')
+
+    def text_future_and_past_questions_presence(self):
+        '''
+        If there are questions from the pasr, and question for future publishing,
+        just past questions must appear in /polls/index
+        '''
+        past_question = create_question('past_question', -30)
+        future_question = create_question('past_question', 30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [past_question]
+        )
+
+    def text_two_questions_from_past_appears(self):
+        '''
+        If there are two questions from past, both must appear
+        in polls/index
+        '''
+        past_question1 = create_question('past_question 1', -30)
+        past_question2 = create_question('past_question 2', -3)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [past_question1, past_question2]
+        )
+
+    def text_two_questions_from_future_not_appears(self):
+        '''
+        If there are two questions for future publishing, neither must appear
+        in polls/index
+        '''
+        future_question1 = create_question('future_question', 30)
+        future_question2 = create_question('future_question', 3)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            []
+        )
